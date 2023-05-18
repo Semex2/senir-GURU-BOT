ea001f8beda7fcb13b44f7f502556fda547d0d08
Semex2 <133959009+Semex2@users.noreply.github.com>
2023-05-18 14:58:37 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94764497079', 'sandaru', true],
-  ['6281255369012', 'Ash', true], 
+  ['94760801056', 'sandaru', true],
+  ['94760801056', 'Ash', true], 
   [''] 
 ] //Number of owners
 
